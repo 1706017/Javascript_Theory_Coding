3f6b8f9e4ec5439ada8aa18ce4489a3b11d28c9a
AMRIT MANASH <amritmanash9@gmail.com>
2026-08-19 10:59:34 +0530

    Implement grade evaluation using else if ladder
    Added an else if ladder to evaluate grades based on marks.

--- Day-05-Conditional_Statements.js
+++ Day-05-Conditional_Statements.js
@@ -14,3 +14,35 @@ else{
 
 //Output for the above code : 
 //You are eligible to vote
+
+//code for else if ladder practice code
+
+let marks=75
+
+if (marks>=90)
+{
+    console.log("You got A grade")
+}
+else if (marks>=80)
+{
+    console.log("You got B grade")
+}
+else if (marks>=70)
+{
+    console.log("You got C grade")
+}
+else if (marks>=60)
+{
+    console.log("You got D grade")
+}
+else if (marks>=50)
+{
+    console.log("You got E grade")
+}
+else{
+    console.log("You got F grade and you are failed")
+}
+
+//Output for the above code:
+
+//You got C grade
