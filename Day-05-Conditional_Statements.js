8633a9ff297a92d53f9a5d735135e36cc6df82e3
AMRIT MANASH <amritmanash9@gmail.com>
2026-08-19 10:54:57 +0530

    Implement if-else statement for voting eligibility
    Added code for practicing conditional statements.

---
+++ Day-05-Conditional_Statements.js
@@ -0,0 +1,16 @@
+//Code for conditional statements
+
+//if else statement practice code 
+
+let age=30
+
+if (age >=18)
+{
+    console.log("You are eligible to vote")
+}
+else{
+    console.log("You are not eligible to vote")
+}
+
+//Output for the above code : 
+//You are eligible to vote
