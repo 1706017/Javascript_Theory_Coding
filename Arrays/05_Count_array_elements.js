98d7b135111333fa5f67802ada7fac1685ee672b
unknown <amritmanash9@gmail.com>
2026-08-29 18:13:11 +0530

    adding few files for arrays

---
+++ Arrays/05_Count_array_elements.js
@@ -0,0 +1,11 @@
+//This program is to count the number of elements in an array 
+
+let arr1=[10,20,30,40,50,60];
+
+let count=0;
+
+for(let i=0;i<arr1.length;i++)
+{
+    count++;
+}
+console.log("Number of elements in the array is:-->",count)
