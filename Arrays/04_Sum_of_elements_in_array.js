a536a8baadc7dd1404b5d17c132889b79a302370
unknown <amritmanash9@gmail.com>
2026-08-29 18:08:56 +0530

    adding few files for arrays

---
+++ Arrays/04_Sum_of_elements_in_array.js
@@ -0,0 +1,11 @@
+//This program is to find the sum of elements in an array 
+
+let arr=[10,20,30,40,50];
+let sum=0;
+
+for(let i=0;i<arr.length;i++)
+{
+    sum=sum+arr[i];
+}
+
+console.log("Sum of elements in the array is:-->",sum)
