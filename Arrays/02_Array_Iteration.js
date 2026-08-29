56571c55dc0a327d149b6b8fb42c32bd656a508f
unknown <amritmanash9@gmail.com>
2026-08-29 16:34:38 +0530

    adding few files for arrays

--- Arrays/02_Array_Iteration.js
+++ Arrays/02_Array_Iteration.js
@@ -7,3 +7,20 @@ console.log("My skills are as follows :--->")
 for(let i=0;i<skills.length;i++){
     console.log(skills[i]);
 }
+
+// for in loop 
+
+let empids =[101,102,103,104]
+
+for(let i in empids){
+    console.log(i); // it will return the index of the array elements 
+    console.log(empids[i]); // it will return the value of the array elements
+}
+
+// for of loop
+
+let empnames = ['John','Smith','David','Peter']
+
+for (let i of empnames){
+    console.log(i); // it will return the value of the array elements
+}
