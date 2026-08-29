6ac5b8107feebe257caca301a429d1c3bc07609b
unknown <amritmanash9@gmail.com>
2026-08-29 16:28:39 +0530

    adding few files for arrays

---
+++ Arrays/01_Creating_an_array.js
@@ -0,0 +1,15 @@
+//Empty array declaration 
+
+let arr = []; // it is an empty array 
+
+console.log(typeof arr); // it will return object because array is a type of object in javascript 
+console.log(arr); // it will return empty array []
+
+let arr1 = [10,20,30];
+console.log(arr1); // it will return [10,20,30]
+console.log(arr1.length); // it will return 3 because there are 3 elements in the array
+
+
+
+
+
---
+++ Arrays/02_Array_Iteration.js
@@ -0,0 +1,9 @@
+
+//Iterating through an array using a for loop 
+
+let skills = ['JS','Playwright','Cypress','Selenium','Java']
+
+console.log("My skills are as follows :--->")
+for(let i=0;i<skills.length;i++){
+    console.log(skills[i]);
+}
