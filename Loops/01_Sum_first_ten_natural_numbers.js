9ba3d8438adc21004f0890f4f27662c5eda89ca5
unknown <amritmanash9@gmail.com>
2026-08-29 15:44:56 +0530

    js_loops_coding_question_commit

---
+++ Loops/01_Sum_first_ten_natural_numbers.js
@@ -0,0 +1,9 @@
+//This code is to find the sum of first 10 natural Numbers 
+
+let sum=0;
+for(let i=1;i<=10;i++)
+{
+  sum= sum + i;
+}
+
+console.log("Sum of First 10 Natural Numbers is :-->",sum)
---
+++ Loops/02_factorial_of_Number.js
@@ -0,0 +1,10 @@
+//This program is to find the factorial of a number 
+
+let fact=1;
+let num=6;
+for(let i=num;i>=1;i--)
+{
+    fact=fact*i;
+}
+console.log(`Factorial of ${num} is :-->`,fact)
+
